6dbc14468bb5e86360fcc07893637ddb7b773e2f
TheSniper15 <sorianojeannebennedict15@gmail.com>
2024-05-17 02:32:34 +0800

    Final

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const searchBtn = document.getElementById("searchBtn");
   const recipesContainer = document.getElementById("recipes");
 
-  searchBtn.addEventListener("click", () => {
+  const fetchRecipes = () => {
     const ingredients = ingredientInput.value.trim();
+    if (!ingredients) return;
 
     const appId = "b880fdf8"; // Edamam API ID
-    
     const appKey = "8dc1f9bad538f070e680c1ce3e7f16eb"; // Edamam API Key
     const apiUrl = `https://api.edamam.com/search?q=${ingredients}&app_id=${appId}&app_key=${appKey}`;
 
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
-        if (data.hits) {
+        if (data.hits.length > 0) {
           displayRecipes(data.hits);
         } else {
           recipesContainer.innerHTML = "<p>No recipes found.</p>";
@@ -22,22 +22,31 @@ document.addEventListener("DOMContentLoaded", () => {
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
+        recipesContainer.innerHTML = "<p>Error fetching recipes. Please try again later.</p>";
       });
+  };
+
+  searchBtn.addEventListener("click", fetchRecipes);
+
+  ingredientInput.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      fetchRecipes();
+    }
   });
 
   function displayRecipes(recipes) {
     recipesContainer.innerHTML = "";
     recipes.forEach((recipe) => {
       const recipeHTML = `
-                <div class="recipe">
-                <div>
-                    <h2>${recipe.recipe.label}</h2>
-                    <p>By ${recipe.recipe.source}</p>
-                    </div>
-                    <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
-                </div>
-                </br>
-            `;
+        <div class="recipe">
+          <div>
+            <h2>${recipe.recipe.label}</h2>
+            <p>By ${recipe.recipe.source}</p>
+          </div>
+          <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
+        </div>
+        <br>
+      `;
       recipesContainer.insertAdjacentHTML("beforeend", recipeHTML);
     });
   }
